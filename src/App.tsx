8aae45165588eeea466ca3cbe4f73f9216d64d14
Rian Rainey <rianrainey@gmail.com>
2024-08-28 14:30:14 -0400

    Lists in react complete

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,25 @@ const welcome = {
   greeting: 'React'
 };
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+]
+
 function getTitle(title: string){
   return title;
 }
@@ -14,6 +33,22 @@ function App() {
 
       <label htmlFor="search">Search: </label> {/* htmlFor allows clicking on label to put focus in input#search */}
       <input id="search" type="text" />
+      <hr />
+      <h1>Articles</h1>
+      <ul>
+        {list.map((item, index) => {
+          return (
+            <li key={index}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   )
 }
