4bf8cc1c91311d263d334fdc0ca424226688da10
Rian Rainey <rianrainey@gmail.com>
2024-08-29 15:21:07 -0400

    Side effect complete

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,26 @@ type Story = {
 const App = () => {
   console.log('App renders');
 
-  const [searchTerm, setSearchTerm] = React.useState('React');
+  const [searchTerm, setSearchTerm] = React.useState(
+    localStorage.getItem('search') || 'React'
+  );
+
+  /*
+    Mutations, subscriptions, timers, logging, and other side effects are not allowed inside the main body of a function component (referred to as React’s render phase).
+    Doing so will lead to confusing bugs and inconsistencies in the UI.
+    Instead, use useEffect. The function passed to useEffect will run after the render is committed to the screen.
+    Think of effects as an escape hatch from React’s purely functional world into the imperative world.
+
+    [searchTerm]: This is the dependency array.
+    The effect will run whenever the values in this array change.
+    In this case, it means that the effect will execute every time searchTerm is updated.
+    If searchTerm remains the same, the effect will not run again.
+  */
+  React.useEffect(() => {
+    localStorage.setItem('search', searchTerm);
+  }, [searchTerm])
+
   const handleSearch = (event:React.ChangeEvent<HTMLInputElement>) => {
-    console.log('In app: ' + event.target.value);
     setSearchTerm(event.target.value);
   }
   const stories = [
